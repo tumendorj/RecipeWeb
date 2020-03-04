8e9a8230b0ce29fab6eb5278cfd574a4b6952e90
tumendorj <ttumendorj@gmail.com>
2020-03-05 00:11:29 +0800

    third commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,30 @@
 import Search from "./model/search";
-let search = new Search("carrot");
-search.doSearch().then(result => console.log(result));
+import { elements } from "./view/base";
+import * as searchView from "./view/searchView";
+/*
+ * web app state
+ * - хайлтын query үр дүн
+ * - үзүүлэх гэж буй жор
+ * - лайкалсан жорууд
+ * - захиалсан жорын найрлагууд
+ */
+const state = {};
+const searchControl = async () => {
+  // 1. input дахь утгыг авна
+  const query = searchView.getInput();
+  if (query) {
+    // 2. hailtiin objectiig uusgeh
+    state.search = new Search(query);
+    // 3. delgetsiin UI beltgeh
+    searchView.clearInput();
+    searchView.clearList();
+    // 4. hailtiig guitsetgene
+    await state.search.doSearch();
+    // 5. hailtiin ur dung delgetsend uzuulne
+    searchView.renderRecipes(state.search.result);
+  }
+};
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  searchControl();
+});
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,2 +1,36 @@
-let name = "tumendorj";
-export default name;
+import { elements } from "./base";
+export const getInput = () => elements.searchInput.value;
+export const renderRecipe = recipe => {
+  //   publisher: "All Recipes";
+  //   title: "Chicken Fettuccini Alfredo";
+  //   source_url: "http://allrecipes.com/Recipe/Chicken-Fettuccini-Alfredo/Detail.aspx";
+  //   recipe_id: "7202";
+  //   image_url: "http://forkify-api.herokuapp.com/images/214411de5a.jpg";
+  //   social_rank: 99.9999995711024;
+  //   publisher_url: "http://allrecipes.com";
+  const markup = `
+     <li>
+        <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+            <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="Test">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${recipe.title}</h4>
+                <p class="results__author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>
+    `;
+  elements.resultList.insertAdjacentHTML("beforeend", markup);
+};
+export const renderRecipes = recipes => {
+  recipes.forEach(el => {
+    renderRecipe(el);
+  });
+};
+export const clearInput = () => {
+  elements.searchInput.value = "";
+};
+export const clearList = () => {
+  elements.resultList.innerHTML = "";
+};
